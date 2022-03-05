2e7cdd2dbb611c5a151a1b4e01b710f23df1ac08
kristaps2044 <76948849+kristaps2044@users.noreply.github.com>
2022-03-05 12:52:17 +0200

    l

--- MakeCV.js
+++ MakeCV.js
@@ -1,6 +1,6 @@
 let inputs = document.querySelectorAll('.input-styles');
-let submit = document.querySelector('.form-window');
-submit.addEventListener('submit', function(e){
+let form = document.querySelector('.form-window');
+form.addEventListener('submit', function(e){
     e.preventDefault();
     for(const values of inputs){
         console.log(values.value)};
