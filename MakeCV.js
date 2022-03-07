3ec79096e57fb682d6217eeb032c25a252a370d6
kristaps2044 <76948849+kristaps2044@users.noreply.github.com>
2022-03-07 19:31:40 +0200

    jaunas lietas

--- MakeCV.js
+++ MakeCV.js
@@ -1,4 +1,5 @@
 let inputs = document.querySelectorAll('.input-styles');
+console.log(inputs);
 let form = document.querySelector('.form-window');
 form.addEventListener('submit', function(e){
     e.preventDefault();
