9d79ad4c8933ecaf8327b457b1a1c9124b6cb6ae
kristaps2044 <76948849+kristaps2044@users.noreply.github.com>
2022-03-12 12:02:26 +0200

    Xd

--- MakeCV.js
+++ MakeCV.js
@@ -5,11 +5,12 @@ let form = document.querySelector('.form-window');
 const TabPreview = document.querySelector('#newTabPreview');
 console.log(inputs);
 
+//Opens new tab on clicking the button next to added image.
 function newTab(){
     const url = preview.src;
     window.open(url);
 };
-
+//When you add image you can prieview it on the window that you clicked on.
 imgInp.onchange = evt => {
     const [file] = imgInp.files
     if (file) {
@@ -17,7 +18,7 @@ imgInp.onchange = evt => {
     }
   };
 
-
+// When you submit you find all elements that are added by user.
 form.addEventListener('submit', function(e){
     e.preventDefault();
     for(const values of inputs){
