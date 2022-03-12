1fe28b5848839884a4aee0c10256c4804ed3fca7
kristaps2044 <76948849+kristaps2044@users.noreply.github.com>
2022-03-12 11:54:42 +0200

    jaun fīča redzi savu bildi ko esi ielicis un to tu vari paskatit jauna taba uzp[iežot uz pogas

--- MakeCV.js
+++ MakeCV.js
@@ -1,8 +1,28 @@
 let inputs = document.querySelectorAll('.input-styles');
-console.log(inputs);
+let imgInp = document.querySelector('#choose-pic');
+let preview = document.querySelector('#inpImg');
 let form = document.querySelector('.form-window');
+const TabPreview = document.querySelector('#newTabPreview');
+console.log(inputs);
+
+function newTab(){
+    const url = preview.src;
+    window.open(url);
+};
+
+imgInp.onchange = evt => {
+    const [file] = imgInp.files
+    if (file) {
+      preview.src = URL.createObjectURL(file)
+    }
+  };
+
+
 form.addEventListener('submit', function(e){
     e.preventDefault();
     for(const values of inputs){
-        console.log(values.value)};
+        console.log(values.value)}
+
+
+
     });
