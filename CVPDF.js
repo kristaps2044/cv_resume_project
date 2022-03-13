1b4d6b2c5cad541a70a9b7c5dcb05c2c39777048
Jenvyas <86479939+Jenvyas@users.noreply.github.com>
2022-03-13 13:04:42 +0200

    ough

--- CVPDF.js
+++ CVPDF.js
@@ -1,6 +1,7 @@
-import {jsPDF} from "jspdf";
+window.jsPDF = window.jspdf.jsPDF;
 const makeCVPDF = ()=>{
     const doc = new jsPDF();
     doc.text("Hello world!", 10, 10);
+    doc.save("a4.pdf");
 }
 makeCVPDF();
