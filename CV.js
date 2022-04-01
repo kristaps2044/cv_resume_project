7319480689c2506ed5930bde3fa1553168a2dd10
Jenvyas <86479939+Jenvyas@users.noreply.github.com>
2022-04-01 22:23:01 +0300

    image is now in cv

--- CV.js
+++ CV.js
@@ -1,10 +1,20 @@
 let CVData = JSON.parse(sessionStorage.getItem('CVData'));
 console.log(CVData);
 let elements = ['fullName','phoneNumber','email','school','faculty','degree','studyProgramme'];
+
 function setData(element,data){
     document.getElementById(element).innerHTML = data;
 }
+
 for(element of elements){
     setData(element,CVData[element]);
 }
+
+const profilePhoto = sessionStorage.getItem("profileImage");
+if(profilePhoto){
+    document.getElementById('profilePicture').setAttribute('src',profilePhoto);
+}
+
+
+
 
--- MakeCV.js
+++ MakeCV.js
@@ -10,20 +10,30 @@ function newTab(){
     const url = preview.src;
     window.open(url);
 };
+
 //When you add image you can prieview it on the window that you clicked on.
 imgInp.onchange = evt => {
     const [file] = imgInp.files
     if (file) {
       preview.src = URL.createObjectURL(file)
+
+      //Saves the image in sessionStorage
+      const reader = new FileReader();
+
+      reader.addEventListener("load",()=>
+      {
+        sessionStorage.setItem("profileImage",reader.result); 
+      });
+      reader.readAsDataURL(file);
     }
   };
 
-// When you submit you find all elements that are added by user.
+//saves all input values to sessionStorage
 form.addEventListener('submit', function(e){
     e.preventDefault();
     let CVData ={};
     for(const input of inputs){
-        CVData[input.id] = input.value;
+      CVData[input.id] = input.value;
     }
     CVData.fullName = CVData.firstName+' '+CVData.lastName;
     sessionStorage.setItem("CVData",JSON.stringify(CVData));
