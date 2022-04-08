cfc3490709c1d8a79f5e454a5cbedf071dd924b2
kristaps2044 <76948849+kristaps2044@users.noreply.github.com>
2022-04-08 18:35:30 +0300

    xd

--- CV.js
+++ CV.js
@@ -1,6 +1,6 @@
 let CVData = JSON.parse(sessionStorage.getItem('CVData'));
 console.log(CVData);
-let elements = ['fullName','phoneNumber','email','school','faculty','degree','studyProgramme'];
+let elements = ['fullName','phoneNumber','email','school','faculty','degree','studyProgramme', 'address'];
 
 function setData(element,data){
     document.getElementById(element).innerHTML = data;
