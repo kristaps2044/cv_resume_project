cdbf9a6ebb62e7331ff9e4c7ef7dfd569e549618
Jenvyas <86479939+Jenvyas@users.noreply.github.com>
2022-05-07 21:42:07 +0300

    cv pievienotas visas lietas ko jus bijat uztaisijusi

--- CV.js
+++ CV.js
@@ -1,13 +1,43 @@
 let CVData = JSON.parse(sessionStorage.getItem('CVData'));
 console.log(CVData);
-let elements = ['fullName','phoneNumber','email','school','faculty','degree','studyProgramme', 'address','aboutYourself', 'startDate', 'endDate', 'latvianRead', 'latvianWrite', 'latvianSpeak'];
+let elements = ['fullName','phoneNumber','email','school','faculty','degree','studyProgramme', 'address','aboutYourself', 'startDate','endDate',
+    'latvianRead', 'latvianWrite', 'latvianSpeak',
+    'russianRead', 'russianWrite','russianSpeak',
+    'englishRead', 'englishWrite', 'englishSpeak'];
+let languageElements = [
+    'latvianRead', 'latvianWrite', 'latvianSpeak',
+    'russianRead', 'russianWrite','russianSpeak',
+    'englishRead', 'englishWrite', 'englishSpeak'
+]
+let latvianGrades ={
+    'excellent':'izcili',
+    'good':'labi',
+    'basic':'pamatzināšanas',
+    "cant":'nevaru',
+    'native':'dzimtā'
+}
 
 function setData(element,data){
-    document.getElementById(element).innerHTML = data;
+    try {
+        document.getElementById(element).innerHTML = data;
+    } catch (error) {
+        console.log(error,element);
+    }
 }
 
 for(element of elements){
-    setData(element,CVData[element]);
+    const storedInfo = CVData[element];
+    if (languageElements.includes(element)) {
+        let grade = latvianGrades[storedInfo]
+        setData(element,grade)
+    }
+    else if (element=='startDate'||element=='endDate') {
+        let newDate = new Date(Date.parse(storedInfo))
+        setData(element,(newDate.getMonth()+'.'+newDate.getFullYear()))
+    }
+    else{
+        setData(element,storedInfo)
+    }
 }
 
 const profilePhoto = sessionStorage.getItem("profileImage");
